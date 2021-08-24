da5acedd386c01e78494342f0f629523283c7cb5
thanhluc <doanthanhluc91bvh@gmail.com>
2021-08-24 11:58:56 +0700

    dasd

--- bgb.py
+++ bgb.py
@@ -1,7 +1,7 @@
 import requests
 
 #the required first parameter of the 'get' method is the 'url':
-x = requests.get('https://w3schools.com/python/demopage.htm')
+x = requests.get('https://api.ipify.org')
 
 #print the response text (the content of the requested file):
 print(x.text)
